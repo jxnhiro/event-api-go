d2fd12799a14713f129de69eda3c05dad587aff0
Kenny Jinhiro <jxnhiro@gmail.com>
2024-01-15 00:30:37 +0700

    Event API v1.0

---
+++ models/event.go
@@ -0,0 +1,89 @@
+package models
+
+import (
+	"time"
+
+	"github.com/jxnhiro/event-api/go/db"
+)
+
+type Event struct {
+	ID int64
+	Name string `binding:"required"`
+	Description string `binding:"required"`
+	Location string `binding:"required"`
+	DateTime time.Time `binding:"required"`
+	UserID int
+}
+
+var events = []Event{}
+
+func (e Event) Save() error {
+	//TODO: Add to database
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?)
+	`
+
+	stmt, err := db.DB.Prepare(query) //statement
+	if err != nil {
+		return err
+	}
+	
+	defer stmt.Close()
+	
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	
+	id, err := result.LastInsertId()
+	e.ID = id
+
+	return err
+} 
+
+func GetAllEvents() ([]Event, error){
+	query := `
+	SELECT * FROM events
+	`
+
+	rows, err := db.DB.Query(query) //query is used to get a lot of rows, exec is used to update, delete, and create
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func GetEventById(id int64) (*Event, error){
+	query := `
+		SELECT * FROM events WHERE id = ?
+	`
+
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
+}
